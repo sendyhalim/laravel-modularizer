0e2d6160d40cbd1dfb3d85924faeaaee70463ae5
sendyHalim <sendy_ubm@yahoo.com>
2014-05-02 16:17:57 +0700

    Add base creator

---
+++ src/Sendy/Modularizer/Creators/Creator.php
@@ -0,0 +1,52 @@
+<?php
+namespace Sendy\Modularizer\Creators;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\Support\Str;
+
+/**
+ * @author Sendy Halim <sendyhalim93@gmail.com>
+ */
+abstract class Creator
+{
+	protected $filesystem;
+
+	public function __construct(Filesystem $f)
+	{
+		$this->filesystem = $f;
+	}
+
+	protected function create($filename, $path, $data)
+	{
+		$path = $this->removeTrailingSlash($path);
+		$path.= "/{$filename}";
+
+		if (!$this->filesystem->exists($path))
+		{
+			return $this->filesystem->put($path, $data);
+		}
+
+		return false;
+	}
+
+	protected function prepareDirectory($path)
+	{
+		if (!$this->filesystem->exists($path))
+		{
+			$this->filesystem->makeDirectory($path, 0777, true);
+			return true;
+		}
+
+		return false;
+	}
+
+	protected function removeTrailingSlash($path)
+	{
+		if (Str::endsWith($path, '/'))
+		{
+			unset($path[strlen($path)-1]);
+		}
+
+		return $path;
+	}
+}
